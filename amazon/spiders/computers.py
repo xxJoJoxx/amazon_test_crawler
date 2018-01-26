5597679a5f0b8072a4d25847a6ab378f6bb99d8c
xxJoJoxx <jklz521@gmail.com>
2018-01-26 01:25:13 +0000

    Added email feature, populated log file with links to traverse

--- amazon/spiders/computers.py
+++ amazon/spiders/computers.py
@@ -1,22 +1,61 @@
 # -*- coding: utf-8 -*-
-import scrapy
-
 from amazon.items import AmazonItem
+from scrapy.spiders import CrawlSpider
+from scrapy import signals
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-class ComputersSpider(scrapy.Spider):
+class ComputersSpider(CrawlSpider):
 
     name = 'computers'
     allowed_domains = ["www.amazon.com"]
-    #links:- 1: AIOs 2: mini pcs 3: tower pcs 4: 2 in 1 laptops 5: traditional laptops
+    #links:- 1: AIOs 2: mini pcs 3: tower pcs 4: 2 in 1 laptops 5: traditional laptops 6-8: tablets 9: fire tabs
     # start_urls = ['https://www.amazon.com/s/ref=lp_13896603011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565098%2Cn%3A13896603011&page=1&ie=UTF8&qid=1516656020',
     #               'https://www.amazon.com/s/ref=lp_13896591011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565098%2Cn%3A13896591011&page=1&ie=UTF8&qid=1516751032',
     #               'https://www.amazon.com/s/ref=lp_13896597011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565098%2Cn%3A13896597011&page=1&ie=UTF8&qid=1516752516',
     #               'https://www.amazon.com/s/ref=lp_13896609011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565108%2Cn%3A13896609011&page=1&ie=UTF8&qid=1516752788',
-    #               'https://www.amazon.com/s/ref=lp_13896615011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565108%2Cn%3A13896615011&page=1&ie=UTF8&qid=1516753193']
-    start_urls =['https://www.amazon.com/s/ref=lp_13896603011_pg_{}?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565098%2Cn%3A13896603011&page={}&ie=UTF8&qid=1516656020'.format(i,i)for i in range(2,6)]
+    #               'https://www.amazon.com/s/ref=lp_13896615011_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A565108%2Cn%3A13896615011&page=1&ie=UTF8&qid=1516753193',
+    #               'https://www.amazon.com/s/ref=sr_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A1232597011%2Cp_n_operating_system_browse-bin%3A3077590011&page=1&ie=UTF8&qid=1516913821&lo=computers',
+    #               'https://www.amazon.com/s/ref=sr_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A1232597011%2Cp_n_operating_system_browse-bin%3A3077591011&page=1&ie=UTF8&qid=1516913663&lo=computers',
+    #               'https://www.amazon.com/s/ref=sr_pg_1?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A1232597011%2Cp_n_operating_system_browse-bin%3A3077595011&page=1&ie=UTF8&qid=1516913665&lo=computers',
+    #               'https://www.amazon.com/s/ref=lp_6669703011_pg_1?rh=n%3A16333372011%2Cn%3A%2116333373011%2Cn%3A2102313011%2Cn%3A6669703011&page=1&ie=UTF8&qid=1516914966']
+    start_urls =['https://www.amazon.com/s/ref=sr_pg_{}?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A541966%2Cn%3A13896617011%2Cn%3A1232597011%2Cp_n_operating_system_browse-bin%3A3077595011&page={}&ie=UTF8&qid=1516917047&lo=computers'.format(i,i)for i in range(2,9)]
+
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(ComputersSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+
+    def spider_closed(self, spider):
+        spider.send_mail("Crawling is complete. Thank you for using me :)", "Scraper Report")
+
+    def send_mail(self, message, title):
+        self.log("Attempting to send email notification.")
+        gmailUser = 'crawl.notify.group@gmail.com'
+        gmailPassword = 'theworldshallknowtruepain'
+        recipient = 'jklz521@gmail.com'
+
+        msg = MIMEMultipart()
+        msg['From'] = gmailUser
+        msg['To'] = recipient
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailUser, gmailPassword)
+        mailServer.sendmail(gmailUser, recipient, msg.as_string())
+        mailServer.close()
+        self.log("Mail sent")
+
     def parse(self, response):
-        items={}
-        asin = response.xpath('//li[@class="s-result-item celwidget  "]')
+        items = {}
+        asin = response.xpath('//li[@class="s-result-item s-result-card-for-container a-declarative celwidget  "]')
         for index, sel in enumerate(asin):
             items[index] = AmazonItem()  # instantiates items class
             id = sel.xpath('//li/@data-asin').extract()
--- amazon/spiders/review_reader.py
+++ amazon/spiders/review_reader.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from scrapy.spiders import CrawlSpider
 from amazon.items import AmazonItem
+from scrapy.spiders import CrawlSpider
+from scrapy import signals
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from scrapy.http.request import Request
 
 from pymongo import MongoClient
@@ -23,19 +26,75 @@ class review_reader(CrawlSpider):
     def start_requests(self):
         with open('log.txt', 'r') as f:
             for url in f.readlines():
-                for i in range(1, 6):
+                for i in range(1, 200):
                     new_url=(url.replace('\n','')+str(i))
                     self.log(new_url)
-                    yield Request(new_url,self.parse)#trying to call first 5 paginations
+                    yield Request(new_url,self.parse)#trying to call first 200 paginations
 
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(review_reader, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_error, signal=signals.spider_error)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+
+    def spider_error(self,failure,response,spider):
+        spider.send_error_alert(self,failure,response,"Alert!!! an error occurred with the spider")
+
+    def spider_closed(self, spider):
+        spider.send_mail("Crawling is complete. Thank you for using me :)", "Scraper Report")
+
+    def send_error_alert(self,failure,response,title):
+        self.log("Attempting to send email notification.")
+        gmailUser = 'crawl.notify.group@gmail.com'
+        gmailPassword = 'theworldshallknowtruepain'
+        recipient = 'jklz521@gmail.com'
+
+        message = str(failure) + str(response)
+        msg = MIMEMultipart()
+        msg['From'] = gmailUser
+        msg['To'] = recipient
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailUser, gmailPassword)
+        mailServer.sendmail(gmailUser, recipient, msg.as_string())
+        mailServer.close()
+        self.log("Mail sent")
+
+
+    def send_mail(self, message, title):
+        self.log("Attempting to send email notification.")
+        gmailUser = 'crawl.notify.group@gmail.com'
+        gmailPassword = 'theworldshallknowtruepain'
+        recipient = 'jklz521@gmail.com'
+
+        msg = MIMEMultipart()
+        msg['From'] = gmailUser
+        msg['To'] = recipient
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+
+        mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmailUser, gmailPassword)
+        mailServer.sendmail(gmailUser, recipient, msg.as_string())
+        mailServer.close()
+        self.log("Mail sent")
 
     def parse(self, response):
         items = {}
         review_list = []
         # response = response.replace(body=response.body.replace('<br>', '\n'))
-        reviews=response.xpath('//div[@data-hook="review"]')
-
-        #loop through sel times to add each comment
+        reviews = response.xpath('//div[@data-hook="review"]')
+        # loop through sel times to add each comment
         for index, sel in enumerate(reviews):
             items[index] = AmazonItem()
             title = sel.xpath('//a[@data-hook ="review-title"]//text()').extract()
@@ -47,9 +106,9 @@ class review_reader(CrawlSpider):
             items[index]['customer_comments_title'] = ''.join(title[index]).strip()
             items[index]['customer_comments_author'] = ''.join(author[index]).strip()
             items[index]['customer_comments_date'] = ''.join(review_date[index]).strip('on')
-            items[index]['customer_comments_rating'] = ''.join(rating[index]).replace('out of 5 stars','')
+            items[index]['customer_comments_rating'] = ''.join(rating[index]).replace('out of 5 stars', '')
             # *** the important bit****
-            items[index]['customer_comments_text'] = ''.join(review_body[index]).strip().replace('<br>',' ').replace('<span data-hook="review-body" class="a-size-base review-text">','').replace('</span>','')
+            items[index]['customer_comments_text'] = ''.join(review_body[index]).strip().replace('<br>', ' ').replace('<span data-hook="review-body" class="a-size-base review-text">', '').replace('</span>', '')
 
             review_list.append(items[index])
 
